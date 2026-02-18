df31b8dad75e09113c9e2f5dd5a9c627923148c9
Agent017-Gaming <fahmida596@gmail.com>
2026-02-18 11:36:15 +0100

    Update

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -2,6 +2,8 @@ package Client;
 
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class MainClient {
@@ -11,6 +13,13 @@ public class MainClient {
 
         try {
             Socket socket = new Socket("localhost", 12345);
+            System.out.println("il client si è connesso al server!");
+
+            OutputStream os = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
+            pw.print("Ciao server!");
+            pw.flush();
+            socket.close();
         } catch (IOException e) {
             System.err.println("Errore nella connessione al server!");
         }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,6 +1,9 @@
 package Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,9 +18,19 @@ public class MainServer {
             Socket clientSocket = server.accept();
             System.out.println("CLIENT: connesso al server!");
             //communicazione
+
+            //Lettura
+            InputStream is = clientSocket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String m = br.readLine();
+            System.out.println("SERVER: il cliente: "+ clientSocket +
+                    "ha scritto '" + m + "'");
+
+            //Scrittura
+
+            //Chiusura
             clientSocket.close();   //chiusura data socket
             server.close();         //chiusura connection socket
-
         } catch (IOException e) {
             System.err.println("Non siamo riusciti ad avviare il server!" +
                     "errore nella creazione del connection socket");
